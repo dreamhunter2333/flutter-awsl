8d38d98179c69b461a96f5a00b9303a1a6718993
jinmu333 <dreamhunter2333@gmail.com>
2021-11-01 19:57:59 +0800

    init: awsl

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,44 +29,73 @@ class Awsl extends StatefulWidget {
 }
 
 class _AwslState extends State<Awsl> {
-  static const _itemsLength = 50;
-
   final _androidRefreshKey = GlobalKey<RefreshIndicatorState>();
-
-  late List<MaterialColor> colors;
-  late List<String> songNames;
+  late final List<String> _awslData = <String>[];
+  late int _awslCount = 0;
 
   @override
   void initState() {
-    _setData();
+    _initData();
     super.initState();
   }
 
-  void _setData() {
-    final wordPair = WordPair.random();
-    colors = [Colors.yellow];
-    songNames = [wordPair.asPascalCase];
+  void _initData() {
+    _awslData.clear();
+    fetchCount();
+    fetchData(10, 0);
+  }
+
+  convertData(List urlList) {
+    for (var url in urlList) {
+      _awslData.add(url["pic_info"]["large"]["url"]);
+    }
+  }
+
+  fetchData(limit, offset) {
+    var url = Uri.parse("http://awsl-py.dev.jcstaff.club/list?limit=" +
+        limit.toString() +
+        "&offset=" +
+        offset.toString());
+    http
+        .get(url, headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        })
+        .then((value) => {
+              for (var url in json.decode(value.body))
+                {_awslData.add(url["pic_info"]["large"]["url"])}
+            })
+        .whenComplete(() => setState(() => {}));
+  }
+
+  fetchCount() {
+    http
+        .get(Uri.parse("http://awsl-py.dev.jcstaff.club/list_count"))
+        .then((value) => _awslCount = int.parse(value.body))
+        .whenComplete(() => setState(() => {}));
+  }
+
+  Widget _buildListView() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _awslCount,
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _awslData.length - 10 &&
+              _awslData.length <= _awslCount) {
+            fetchData(10, index);
+          }
+          if (index >= _awslData.length) return Container();
+          return Image.network(_awslData[index]);
+        });
   }
 
   Future<void> _refreshData() {
     return Future.delayed(
       // This is just an arbitrary delay that simulates some network activity.
       const Duration(seconds: 2),
-      () => setState(() => _setData()),
-    );
-  }
-
-  Widget _listBuilder(BuildContext context, int index) {
-    if (index >= _itemsLength) return Container();
-    final wordPair = WordPair.random();
-
-    return SafeArea(
-      top: false,
-      bottom: false,
-      child: Hero(
-        tag: index,
-        child: Text(wordPair.asPascalCase),
-      ),
+      () => setState(() => _initData()),
     );
   }
 
@@ -88,11 +115,7 @@ class _AwslState extends State<Awsl> {
       body: RefreshIndicator(
         key: _androidRefreshKey,
         onRefresh: _refreshData,
-        child: ListView.builder(
-          padding: const EdgeInsets.symmetric(vertical: 12),
-          itemCount: _itemsLength,
-          itemBuilder: _listBuilder,
-        ),
+        child: _buildListView(),
       ),
     );
   }
